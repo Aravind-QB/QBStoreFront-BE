d9d9f23e1f0e075cddb742f030c270df5e184c6c
Preethi <preethi.p@qburst.com>
2023-09-04 12:52:38 +0530

    Added JWT Authentication

--- src/bff/api/defaultRouter.ts
+++ src/bff/api/defaultRouter.ts
@@ -1,24 +1,32 @@
 import { Router } from 'express';
 import { PLATFORM } from '../config';
+import { authenticateToken ,generateAccessToken } from '../middleware';
 const apiMethods = require(`../../${PLATFORM}/api`);
-
 const router = Router();
+const jwt = require('jsonwebtoken');
+
+const dotenv = require('dotenv')
+dotenv.config();
+
+process.env.TOKEN_SECRET;
+  
+router.post('/api/login', (req, res) => {
+    const token = generateAccessToken({ username: req.body.username });  
+    console.log(token)
+    res.json(token);
+     
+});
 
 export const V1DefaultRoutes = () => {
 
+
     router.get(
-        '/default', (req, res) => {
-            const response = apiMethods.defaultMethod(req);
+        '/default', authenticateToken, (req, res) => {
+            const response = apiMethods.defaultMethod();
             res.send(response);
         }
     );
 
-    router.post(
-        '/default/:id', (req, res) => {
-            const response = apiMethods.defaultMethod(req);
-            res.send(response);
-        }
-    );
 
     return router;
 };
---
+++ src/bff/middleware/jwt.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+const jwt = require('jsonwebtoken');
+
+export function generateAccessToken(username : any) {
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+   
+export function authenticateToken(req : Request, res : Response, next : NextFunction) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+ 
+    if (token == null)
+      return res.sendStatus(401)
+ 
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
+     
+      console.log(err)
+ 
+      if (err)
+          return res.sendStatus(403)
+ 
+      next()
+    })
+  }
+
